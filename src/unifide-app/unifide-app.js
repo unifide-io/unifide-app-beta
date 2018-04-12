bce3ed98a1ea14b6c35baa61fc378f67eaf2d00f
mcmillennick <mcmillennick@gmail.com>
2018-04-12 02:07:56 -0700

    app mockup sprint 1
    started the design and development of our html mockup. this code will be used as the app framework to speed up development

--- src/unifide-app/unifide-app.js
+++ src/unifide-app/unifide-app.js
@@ -7,14 +7,28 @@ import * as view from './unifide-app.template.html';
 //Import Dependencies
 //polymer app elements
 import '@polymer/app-layout/app-layout.js';
+import '@polymer/app-route/app-route.js';
+import '@polymer/app-route/app-location.js';
+
 //polymer iron elements
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-icons/av-icons.js';
 import '@polymer/iron-icons/social-icons.js';
 import '@polymer/iron-icons/hardware-icons.js';
+import '@polymer/iron-pages/iron-pages.js';
+
 //polymer paper-elements
+import '@polymer/paper-card/paper-card.js';
 import '@polymer/paper-listbox/paper-listbox.js';
 import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
+
+//unifide elements
+import './unifide-icons.js';
+
+//unifide ui elements
+import 'gridstack/dist/gridstack.js';
+
 
 
 class UnifideApp extends PolymerElement {
@@ -25,6 +39,12 @@ class UnifideApp extends PolymerElement {
           type: String,
           value: 'Unifide'
         },
+        appRoute: {
+          type: String,
+        },
+        isAnonymous: {
+          type: Boolean,
+        }
 
       }
   }
